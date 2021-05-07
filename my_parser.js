f9ba0a1a9123cd1b4212b2b9cdd9f9bdd013a53c
Andrejs Naumovs <andrejs@naumovs.de>
2021-05-07 13:36:58 +0200

    first durak commit

---
+++ my_parser.js
@@ -0,0 +1,29 @@
+#!javascript
+// LOad the fs (filesystem) module
+var fs = require ('fs');
+// read the contents of the file into memory
+fs.readFile('example_log.txt', function(err, logData){
+    // If an error occurred, throwing it will
+    // display the exception and end our app
+    if(err) throw err;
+    // logData is a Buffer, convert to string
+    var text = logData.toString();
+
+    // parse to result
+    var results = {};
+    // break up the files to the lines
+    var lines = text.split('\n');
+    lines.forEach(line => {
+        var parts = line.split(' ');
+        var letter = parts[1];
+        var count = parseInt(parts[2]);
+        if(!results[letter]) {
+            results[letter] = 0;
+        }
+        // add count
+        results[letter] += parseInt(count);
+
+    });
+
+    console.log(results);
+})
